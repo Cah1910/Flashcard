6077792f2752f05304faa50f33663b91fd2be950
Cah1910 <00001096758763sp@al.educacao.sp.gov.br>
2024-11-27 15:32:06 -0300

    Projeto Concluído
    Primeira versão

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao (
+    'Corinthians',
+    'O que significa a sigla SCCP?',
+    'Sport Club Corinthians Paulista'
+)
+
+criaCartao (
+    'Corinthians',
+    '​Que dia foi o primeiro jogo na Arena Corinthians?',
+    '18 de Maio de 2014'
+)
+
+criaCartao (
+    'Corinthians',
+    '​Quantas Copas do Brasil o Corinthians tem atualmente?',
+    '3 copas'
+)
+
+criaCartao (
+    'Corinthians',
+    '​Qual é o jogador que mais vezes vestiu a camisa do Corinthians?',
+    'Wladimir'
+)
+
+criaCartao (
+    'Corinthians',
+    'Qual foi o primeiro jogo do Corinthians no Pacaembu?',
+    'Atlético-mg'
+)
+
+criaCartao (
+    'Corinthians',
+    '​Qual gigante europeu o Corinthians eliminou para chegar a final do Mundial de clubes da FIFA em 2000?',
+    'Chelsea'
+)
+
+criaCartao (
+    'Corinthians',
+    'Quais foram, respectivamente, os adversários do Corinthians na fase mata-mata da Taça Libertadores 2012?',
+    'Emelec (Equador), Vasco (Brasil), Santos (Brasil) e Boca Jrs (Argentina)'
+)
+
+criaCartao (
+    'Corinthians',
+    'Quantas vezes o Corinthians foi campeão da Copa Libertadores Feminina?',
+    'O Corinthians é o maior vencedor da Copa Libertadores Feminina, tendo conquistado o pentacampeonato'
+)
+
+criaCartao (
+    'Corinthians',
+    '​Quantos títulos o Corinthians tem no Brasileirão Feminino?',
+    'O Corinthians tem seis títulos do Brasileirão Feminino: 2018, 2020, 2021, 2022, 2023 e 2024'
+)
